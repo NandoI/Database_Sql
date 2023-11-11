a39667cce66180420c8c01c369f8f90a477d3ee9
Faishal Fernando Hutama <125978885+NandoI@users.noreply.github.com>
2023-11-11 14:41:58 +0700

    mengupdate data dalam table yang tadi lupa disave

---
+++ memilihTable_UpdateTable.sql
@@ -0,0 +1,7 @@
+SELECT * FROM User;
+SELECT * FROM Product;
+SELECT * FROM Orders;
+
+UPDATE User SET nama = 'Masa Sudah Di Update' where user_id = 3;
+UPDATE Product SET product_name = "Coca-Terupdate" WHERE product_id = 4;
+UPDATE Orders SET jumlah = 8 WHERE order_id = 1;
